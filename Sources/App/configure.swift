f84d30025f733ba1f743d1dcef0308798dd13fd6
Kushal Ashok <kushal.ashok@gmail.com>
2023-08-16 00:14:49 +0800

    First commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,9 +2,11 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    // Serves files from `Public/` directory
+    let fileMiddleware = FileMiddleware(
+        publicDirectory: app.directory.publicDirectory
+    )
+    app.middleware.use(fileMiddleware)
     // register routes
     try routes(app)
 }
